9d53153072efd7138fcaadf1e3e3877ed1d16891
Daris Omericic <darisomericicpg@gmail.com>
2025-11-22 17:17:35 +0100

    Database-kobling mappe

--- app.py
+++ app.py
@@ -22,12 +22,9 @@ def index():
             connect.commit()
             connect.close()
             
-            
-    
-    
     return render_template('index.html')
 
 
  
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
