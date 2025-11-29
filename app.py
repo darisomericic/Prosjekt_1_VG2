1cfefa0975f53e62443d59a00eb9da2850a2d2b6
Daris Omericic <darisomericicpg@gmail.com>
2025-11-29 16:22:38 +0100

    Oppdatert nettside med ny funksjonalitet

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+import requests 
 import mariadb
 
+load_dotenv()
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
@@ -24,7 +31,21 @@ def index():
             
     return render_template('index.html')
 
+API_KEY = os.getenv('API_KEY')
+
+@app.get('/weather')
+def weather():
+    By = request.args.get('by')
+    Land = request.args.get('land')
+    
+    if not By or not Land:
+        return jsonify({ "error": 'Vennligst oppgi både by og land'}), 400
+    
+    url = f"https://api.weatherapi.com/v1/current.json?key={API_KEY}&q={By},{Land}"
+    response = requests.get(url).json()
+    
+    return jsonify(response)
 
  
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE search_stats (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    city VARCHAR(100),
+    country VARCHAR(100),
+    count INT DEFAULT 1
+);
