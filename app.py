cd630d710f354347897444177ae5bf6e5f2ea220
Daris Omericic <darisomericicpg@gmail.com>
2025-12-03 11:34:47 +0100

    oppdatert database

--- app.py
+++ app.py
@@ -10,26 +10,14 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods = ['GET', 'POST'])
-def index():
-    connect = None
-    try:
-        connect = mariadb.connect(
+
+def db_connection(): 
+        return mariadb.connect(
            host=os.getenv('DB_HOST'),
            user=os.getenv('DB_USER'),
            password=os.getenv('DB_PASS'),
            database=os.getenv('DB_NAME')
         )
-        cursor = connect.cursor() 
-        cursor.execute()
-    except Exception as e:
-        print('Error:', e)
-    finally:
-        if connect is not None:
-            connect.commit()
-            connect.close()
-            
-    return render_template('index.html')
 
 API_KEY = os.getenv('API_KEY')
 
@@ -40,10 +28,54 @@ def weather():
     
     if not By or not Land:
         return jsonify({ "error": 'Vennligst oppgi både by og land'}), 400
+ 
+     
+    return søk_statistikk(By, Land)
+    
+    
+def søk_statistikk(By, Land):
     
     url = f"https://api.weatherapi.com/v1/current.json?key={API_KEY}&q={By},{Land}"
     response = requests.get(url).json()
+
+
+# Sjekk om API-et gir error
+    if response.get('error'):
+        return jsonify({"error": 'Ugyldig by eller land'}), 400
+
+# Hent landet som API-et sier byen ligger i
+    api_land = response.get('location', {}).get('country')
+
+# Sjekk om det matcher brukerens input (case-insensitive)
+    if api_land.lower() != Land.lower():
+        return jsonify({"error": f"Byen {By} finnes ikke i {Land}"}), 400
     
+    Forbokstav_Land = Land.strip().capitalize()
+    Forbokstav_By = By.strip().capitalize()
+
+
+
+
+    connection = db_connection()
+    cursor = connection.cursor()
+    
+    
+    cursor.execute("""
+                   INSERT INTO søk_statistikk (By_navn, Land_navn, Antall_ganger_søkt)
+                   VALUES (%s, %s, 1)
+                   ON DUPLICATE KEY UPDATE Antall_ganger_søkt = Antall_ganger_søkt + 1
+                """, (Forbokstav_By, Forbokstav_Land))
+    connection.commit() 
+    
+    cursor.execute("SELECT Antall_ganger_søkt FROM søk_statistikk WHERE By_navn = %s AND Land_navn = %s", (Forbokstav_By, Forbokstav_Land))
+    row = cursor.fetchone()
+    antall_søk = row[0] - 1 if row else 0
+    
+    cursor.close()
+    connection.close()
+
+    
+    response['antall_søk'] = antall_søk
     return jsonify(response)
 
  
--- db.sql
+++ db.sql
@@ -1,6 +1,7 @@
-CREATE TABLE search_stats (
+CREATE TABLE søk_statistikk (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    city VARCHAR(100),
-    country VARCHAR(100),
-    count INT DEFAULT 1
+    By_navn VARCHAR(255),
+    Land_navn VARCHAR(255),
+    Antall_ganger_søkt INT NOT NULL DEFAULT 0,
+    UNIQUE KEY unik_by_land (By_navn, Land_navn) 
 );
