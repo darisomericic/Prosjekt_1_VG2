3d76affe499a42aba7fec0393c9b30a123c24a32
Daris Omericic <darisomericicpg@gmail.com>
2025-12-03 11:37:47 +0100

    oppdatert frontend og responsivitet

--- App.jsx
+++ App.jsx
@@ -1,6 +1,5 @@
 import styles from './vær.module.css'
-import sky from '../vaermeldingsapp/img/blurredsky.png'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 
 
@@ -8,38 +7,35 @@ function VærApp() {
   const [Land, setLand ] = useState("")
   const [By, setBy ] = useState("")
   const [Vær, setVæret ] = useState(null)
-  const [Forecast, setForecast ] = useState([]) 
    
     const handleSubmit = async (e) => {
     e.preventDefault();
+    await getVæret();
     }
 
 const getVæret = async () => {
  if (!By || !Land) {
-  alert('Skriv inn både by og land!')
+  alert('Fyll ut begge felt!')
   return;
 } 
+
   
-     
-  const response = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=c38fbade3d1f4f1e94d102035252011&q=${By},${Land}`
-  );
-  const json = await response.json();
+  try {
+    const response = await fetch(
+      `http://10.2.223.200:5000/weather?by=${By}&land=${Land}`);
+    const json = await response.json();
 
-   if (json.location.country.toLowerCase() !== Land.toLowerCase()) {
-    alert("Skriv inn riktig by!");
-    return;
+    if (json.location.country.trim().toLowerCase() !== Land.trim().toLowerCase()) { 
+      alert("Skriv inn riktig land eller by!"); 
+      return; 
+    } 
+
+  
+    setVæret(json);
+  } catch (error) {
+    console.error("Feil i fetch:", error);
+    alert("Noe gikk galt med å hente værdata.");
   }
-
-  const getForecast = async () => {
-      const response = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=c38fbade3d1f4f1e94d102035252011&q=${By},${Land}`
-  );
-  const json = await response.json()
-  setForecast(json.forecast.forecastday)
-  }
-
-  setVæret(json);
 };
 
 
@@ -49,21 +45,25 @@ const getVæret = async () => {
     return (
       <form action="" onSubmit={handleSubmit} className={styles.container}>
           <div className={styles.innhold}>
-            <input type="text" style={{ fontStyle: 'italic' }} placeholder='Skriv inn et land...' className={styles.innhold} onChange={(e) => setLand(e.target.value)}></input>
-            <input type="text" style={{ fontStyle: 'italic' }} placeholder='Skriv inn et sted...' className={styles.innhold} onChange={(e) => setBy(e.target.value)}></input>
-            <button className={styles.btn} type='button' onClick={getVæret}>Søk</button>
+            <input type="text" style={{ fontStyle: 'italic' }} placeholder='Enter a country...' value={Land} className={styles.innhold} onChange={(e) => setLand(e.target.value)}></input>
+            <input type="text" style={{ fontStyle: 'italic' }} placeholder='Enter a place...' value={By} className={styles.innhold} onChange={(e) => setBy(e.target.value)}></input>
+            <button className={styles.btn} type='submit'>Søk</button>
              
               {Vær?.location && Vær?.current && (
               <div className={styles.weatherbox}>
               <h3>{Vær.location.name}, {Vær.location.country}</h3>
-              <p><b>Temperatur:</b> {Vær.current.temp_c}°C</p>
-              <p><b>Vær:</b> {Vær.current.condition.text}</p>
-              <p><b>Fuktighet:</b> {Vær.current.humidity}</p>
-              <p><b>Vindstyrke:</b> {Vær.current.wind_kph} km/h</p>
-              <p><b>Tid:</b> {Vær.location.localtime}</p>
+              <p><b>Temperature:</b> {Vær.current.temp_c}°C</p>
+              <p><b>Condition:</b> {Vær.current.condition.text}</p>
+              <p><b>Humidity:</b> {Vær.current.humidity}</p>
+              <p><b>Wind:</b> {Vær.current.wind_kph} km/h</p>
+              <p><b>Time:</b> {Vær.location.localtime}</p>
               <div className={styles.bilde}>
-              <img id='current-icon'src={`https:${Vær.current.condition.icon}`} alt="Værikon"  />
+              <img id='current-icon'src={`https:${Vær.current.condition.icon}`} alt="Værikon"  />      
+              <div className={styles.searchHistory}> 
+                <b>{Vær.antall_søk} other have searched this</b>
+              </div>     
               </div>
+              
         </div>
               )}
           </div>
